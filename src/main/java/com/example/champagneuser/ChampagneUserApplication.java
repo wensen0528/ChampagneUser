bc09ee137ad024666fca6928cca3d0c247a7eed5
2465762554@qq.com <wensen@wensendeAir.lan>
2023-06-26 22:49:50 +0800

    Initial commit

---
+++ src/main/java/com/example/champagneuser/ChampagneUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.champagneuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampagneUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampagneUserApplication.class, args);
+	}
+
+}
