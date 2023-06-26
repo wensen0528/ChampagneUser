fc1c1f14d619e4f65a6a46e2f4e6e974d705b6d4
wensen <2465762554@qq.com>
2023-06-26 23:00:03 +0800

    tft

--- src/main/java/com/example/champagneuser/ChampagneUserApplication.java
+++ src/main/java/com/example/champagneuser/ChampagneUserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChampagneUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChampagneUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChampagneUserApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/champagneuser/ChampagneUserApplicationTests.java
+++ src/test/java/com/example/champagneuser/ChampagneUserApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.champagneuser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ChampagneUserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+    }
 
 }
